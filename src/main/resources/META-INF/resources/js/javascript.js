10efaae7f959d4f427f185715a885f0a0b8d244e
ThiagoRLimaa <thiago.robe@live.com>
2022-11-28 16:36:06 -0300

    confirmar

--- src/main/resources/META-INF/resources/js/javascript.js
+++ src/main/resources/META-INF/resources/js/javascript.js
@@ -1,35 +1,14 @@
-function cadastrar() {
+function CadastrarProduto(){
+        produtos = {};
+        produtos.descricao = document.getElementById("txtDescricao").value;
+        produtos.vaor = document.getElementById("valor").value;
+      
     
-    presenca.codpessoa = document.getElementById("txtNome").value;
-    presenca.nomecurso = document.getElementById("TxtCpf").value;
-
-    axios.post("http://localhost:8080/pessoa", pessoa)
-    .then(function (response) {
-        alert("Cadastro realizado com sucesso");        
-    })
-    .catch(function (error) {
-        alert("Cadastro não efetuado");     
-
-    });    
-}
-
-function InserirSelect(){
-    axios.get('http://localhost:8080/presenca/',{})
-    .then(function (response) {
-        preencherSelect(response.data);
+        axios.post("http://localhost:8080/Produtos", produtos)
+        .then(function (response) {
+            limparFormulario();
         })
-}
-
-
-function preencherSelect(dados){
-    let selectTabela = document.getElementById("selecionar");
-    selectTabela.options = [];
-    selectTabela.options.length = 0;
-    option = new Option("Selecione o Item",null);
-    selectTabela.options[selectTabela.options.length] = option
-    dados.forEach((presenca) => {
-        option = new Option(presenca.CODPESSOA, presenca.CODPESSOA);
-        selectTabela.options[selectTabela.options.length] = option;
-    });
-   
-}
+        .catch(function (error) {
+    
+        });
+    }
